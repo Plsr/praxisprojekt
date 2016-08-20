06c25fa23816fea8d7aeeb4479fa79fee9cc4418
plsr <christianpoplawski@gmail.com>
2016-08-20 20:17:52 +0200

    Text example now resized to fit entered number
    Also added some documentation

--- PoC/typographie/scripts/calculate.js
+++ PoC/typographie/scripts/calculate.js
@@ -1,8 +1,8 @@
 function calc() {
-  var textSize = document.getElementById('font-size').value;
-  var lineHeight = Math.round(textSize * 1.4);
+  var fontSize = document.getElementById('font-size').value;
+  var lineHeight = Math.round(fontSize * 1.4);
 
-  console.log('Text Size: ' + textSize);
+  console.log('Text Size: ' + fontSize);
   console.log('Line Height: ' + lineHeight);
 
   // Experimantal
@@ -10,8 +10,8 @@ function calc() {
   var ws = document.getElementById('ws');
   var is = document.getElementById('is');
 
-  var wCharWidth = getCharWidth(adjustElementWidth(ws));
-  var iCharWidth = getCharWidth(adjustElementWidth(is));
+  var wCharWidth = getCharWidth(adjustElementWidth(ws, fontSize));
+  var iCharWidth = getCharWidth(adjustElementWidth(is, fontSize));
 
   var averageWidth = (wCharWidth + iCharWidth) / 2;
   console.log(averageWidth);
@@ -25,9 +25,17 @@ function calc() {
  *  Fills an Element with Characters so that the Element is as close as possible
  *  to a width of 300px.
  */
-function adjustElementWidth(element) {
+function adjustElementWidth(element, fontSize) {
+  // TODO: Verify variables
+
+  // Remove whitespace from text
   var elementContent = element.innerHTML.replace(/\s/g, '');
 
+  // Set font size of text example according to what
+  // the user defined
+  element.style.fontSize = fontSize + "px";
+
+  // DEBUG
   console.log(elementContent);
   console.log('Width of Element at start: ' + element.offsetWidth);
   console.log('Content length at start: ' + elementContent.length);
@@ -35,13 +43,17 @@ function adjustElementWidth(element) {
   // Calculate width of one character
   var singleCharWidth = getCharWidth(element);
   console.log('Width of a single character: ' + singleCharWidth);
+
+  // Calculate how many chars will fit in 300px width
   var numChars = Math.floor((300 - (300 % singleCharWidth)) / singleCharWidth);
   console.log(numChars);
 
+  // Create new string according to calculated values
   var newContent = elementContent.substring(0, numChars);
   console.log(newContent);
   console.log('Measured length of new Content: ' + newContent.length);
 
+  // Populate Element with new content
   element.innerHTML = newContent;
   console.log(element.offsetWidth);
 
@@ -53,6 +65,8 @@ function adjustElementWidth(element) {
  *  Calculates the average width of a single character in the given element.
  */
 function getCharWidth(element) {
+  // TODO: Verify variables
+
   var elementContent = element.innerHTML.replace(/\s/g, '');
   var elementLength = element.innerHTML.length; // should always be 5
   var elementWidth = element.offsetWidth;
