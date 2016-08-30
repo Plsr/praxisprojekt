148564013785318e25378b5a0357c71381f9fbb9
plsr <christianpoplawski@gmail.com>
2016-08-30 07:53:22 +0200

    Added some documentation

--- PoC/typographie/scripts/colors.js
+++ PoC/typographie/scripts/colors.js
@@ -19,7 +19,15 @@ $(document).ready(function() {
     }
   });
 
+
+  /*
+   *  Check contrast of the two currently picked colors
+   *  based on WCAG 2.0 G18
+   *  https://www.w3.org/TR/2016/NOTE-WCAG20-TECHS-20160317/G18
+   */
   function checkContrast() {
+    // Setup
+    // Get currently set colors
     var bgColor = $("#bg-color").spectrum("get").toRgb();
     var fgColor = $("#fg-color").spectrum("get").toRgb();
     var colors = [bgColor, fgColor];
@@ -28,10 +36,14 @@ $(document).ready(function() {
     for(var i = 0; i <colors.length; i++) {
       var currColor = colors[i];
 
+      // Convert RGB values of colors to sRGB
+      // Then do claculations as specified in WCAG 2.0 G18
+      // https://www.w3.org/TR/2016/NOTE-WCAG20-TECHS-20160317/G18#G18-tests
       for (var singleColor in currColor) {
         if (currColor.hasOwnProperty(singleColor)) {
           console.log(currColor[singleColor]);
           currColor[singleColor] = currColor[singleColor] / 255;
+
           if (currColor[singleColor] <= 0.03928) {
             currColor[singleColor] = currColor[singleColor] / 12.92;
           } else {
@@ -40,6 +52,7 @@ $(document).ready(function() {
         }
       }
 
+      // Store luminances of foreground and background color
       lumis.push((0.2126 * currColor.r + 0.7152 * currColor.g + 0.0722 * currColor.b) + 0.05);
     }
 
@@ -53,18 +66,15 @@ $(document).ready(function() {
     console.log("Raw luminance: ");
     console.log(lumis);
 
-    var ratio;
-
+    // Normalize luminances so that the darker one is 1
     if(lumis[0] > lumis[1]) {
       var multiplicator = 1 / lumis[1];
       lumis[0] = lumis[0] * multiplicator;
-      lumis[1] = lumis[1] * multiplicator;
-      ratio = lumis[0];
+      var ratio = lumis[0];
     } else {
       var multiplicator = 1 / lumis[0];
-      lumis[0] = lumis[0] * multiplicator;
       lumis[1] = lumis[1] * multiplicator;
-      ratio = lumis[1];
+      var ratio = lumis[1];
     }
 
     // DEBUG
@@ -74,6 +84,9 @@ $(document).ready(function() {
     console.log("Ratio: ");
     console.log(ratio);
 
+    // Check the ratio of the lighter color
+    // To pass WCAG 2.0 AA it has to be at least 4.5
+    // To pass WCAG 2.0 AAA it has to be at least 7
     if(ratio >= 7) {
       console.log("Passing AAA");
     } else if (ratio >= 4.5) {
